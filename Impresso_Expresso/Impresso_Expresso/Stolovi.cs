03bf46c53e00895e7e86d22f92b6004dc87fc9b4
DESKTOP-B98S5GO\vicke <vickedvuljo@gmail.com>
2018-04-20 14:50:55 +0200

    Dodavanje klase Stolovi

---
+++ Impresso_Expresso/Impresso_Expresso/Stolovi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impresso_Expresso
+{
+    class Stolovi
+    {
+        public int idStola { get; set; }
+    }
+}
